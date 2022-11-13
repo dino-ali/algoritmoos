a4cb8381c72b39739973a29606b50739133519df
sofiacarrera <72933669+sofiacarrera@users.noreply.github.com>
2022-11-13 17:50:53 -0500

    Update buffer.hpp

--- buffer.hpp
+++ buffer.hpp
@@ -3,3 +3,28 @@
 ------------------
 This file defines the interfaces for the EditorBuffer Class
 //zapato :).
+*/
+
+#ifndef _buffer_h
+#define _buffer_h
+
+//Class editor buffer
+
+class EditorBuffer{
+public: 
+  EditorBuffer();
+  ~EditorBuffer();
+  
+  void moveCursorForward();
+  void moveCursorBackward();
+  
+  void moveCursorToStart();
+  void moveCursosToEnd();
+  
+  void insertCharacter(char ch);
+  void deleteCharacter();
+  
+  
+  std::string getText() const;
+  int getCursor() const;
+  
