79a49c995df5ea9d923a6fdc71b1d617900e4194
sofiacarrera <72933669+sofiacarrera@users.noreply.github.com>
2022-11-13 19:00:50 -0500

    Update buffer.hpp

--- buffer.hpp
+++ buffer.hpp
@@ -3,11 +3,13 @@
 ------------------
 This file defines the interfaces for the EditorBuffer Class
 //zapato :).
+OJO NOTAS PARA NOSOTRAS AL FINAL
+1. Revisar si hay necesidad de redireccionar el cursor !!!
 */
 
 #ifndef _buffer_h
 #define _buffer_h
-
+#include <stack>
 //Class editor buffer
 
 class EditorBuffer{
@@ -15,30 +17,67 @@ public:
   EditorBuffer();
   ~EditorBuffer();
   
-  void moveCursorForward();
-  void moveCursorBackward();
+  void moveCursorForward();//wiiiiii
+  void moveCursorBackward();//wiiiiiiii
   
   void moveCursorToStart();
   void moveCursosToEnd();
   
-  void insertCharacter(char ch);
-  void deleteCharacter();
+  void insertCharacter(char ch);//wii
+  void deleteCharacter();//wii
   
   
   std::string getText() const;
   int getCursor() const;
   
 private:
-  CharStack before();
-  CharStack after();
+  std::stack<char> before();//wii
+  std::stack<char> after();//wii
   
   EditorBuffer(const EditorBuffer & value) { }
   const EditorBuffer & operator=(const EditorBuffer & rhs) {return *this;}
 };
 
 EditorBuffer EditorBuffer(){
-  before
-  after
+  std::stack<char> before;
+  std::stack<char> after;
+  int *cursor = before.top();
+}
+~EditorBuffer(){
+  int b_size = before.size();
+  int a_size = after.size();
+  for (int i = 0; i<b_size;i++{
+    before.pop();
+  }
+  for (int j=0; j<a_size();j++){
+    after.pop();
+  }
+  cursor = nullptr;
+}
+       
+void insertCharacter(char ch){
+  before.push(ch);
+  cursor = before.top();
+}
+       
+void deleteCharacter(){
+  before.pop();
+  cursor = before.top();
+}
+       
+void moveCursorForward(){
+  char a = after.top();
+  before.push(a);
+  after.pop();
+  cursor = before.top() 
+} 
+       
+void moveCursorBackward(){
+  char a = after.top();
+  after.push(a);
+  before.pop();
+  cursor = before.pop();
 }
 
-#endif
+void moveCursorToStart(){}
+  #endif
